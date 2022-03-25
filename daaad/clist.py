dfa16aa697ff3d6fabc3ac2efe96f8bc76147d5a
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2022-03-25 19:56:34 +0430

    Update patterns

--- daaad/clist.py
+++ daaad/clist.py
@@ -15,12 +15,12 @@ _ALLOWED_PATTERNS = defaultdict(list)
 _ALLOWED_PATTERNS["codeforces.com"] = [""]
 _ALLOWED_PATTERNS["atcoder.jp"] = ["beginner", "regular", "grand"]
 _ALLOWED_PATTERNS["hsin.hr/coci"] = [""]
-_ALLOWED_PATTERNS["codingcompetitions.withgoogle.com"] = [""]
 _ALLOWED_PATTERNS["stats.ioinformatics.org"] = [""]
 _ALLOWED_PATTERNS["usaco.org"] = [""]
 
 _DISALLOWED_PATTERNS = defaultdict(list)
 _DISALLOWED_PATTERNS["codeforces.com"] = ["unrated", "kotlin"]
+_DISALLOWED_PATTERNS["usaco.org"] = ["ioi"]
 
 _DAYS = 2
 
